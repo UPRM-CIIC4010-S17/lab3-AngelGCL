522fa82587b375d2d5a7884063077c811b278321
4015d4 <4015d4@platano.ece.uprm.edu>
2017-02-08 14:36:23 -0400

    Changes made:
    -Resized frame, retitled, relocated.
    -Application will now stop running when window is closed.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -5,7 +5,7 @@ public class Lab3Exercises {
 		JFrame myFrame = new JFrame("Angel G. Carrillo Laguna");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(855, 400);
-		myFrame.setSize(400, 400);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
